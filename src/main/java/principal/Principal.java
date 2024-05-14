574d325e1c10a45085203413a258e3b75d5f948b
João P Broering <>
2024-05-14 10:19:38 -0300

    fix: remove principal comments

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package principal;
 
-/**
- *
- * @author 10724112106
- */
 public class Principal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("hello world");
     }
 }
