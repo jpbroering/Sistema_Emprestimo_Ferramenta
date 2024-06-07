4f5996f1903ad4ccf6ab4c36aebd6dba9dd949b1
S4tor11 <home@Sato>
2024-06-06 21:13:01 -0300

    Add: FrmRelatorio Emprestimo

---
+++ src/main/java/visao/FrmRelatorioEmprestimo.java
@@ -0,0 +1,137 @@
+package visao;
+
+import java.util.ArrayList;
+import modelo.Amigo;
+import modelo.Emprestimo;
+import modelo.Ferramenta;
+
+public class FrmRelatorioEmprestimo extends javax.swing.JFrame {
+
+    Ferramenta objetoFerramenta;
+    Emprestimo objetoEmprestimo;
+    Amigo objetoAmigo;
+    ArrayList<Ferramenta> listaFerramenta;
+    ArrayList<Emprestimo> listaEmprestimo;
+    ArrayList<Amigo> listaAmigo;
+
+    public FrmRelatorioEmprestimo() {
+        initComponents();
+        objetoFerramenta = new Ferramenta();
+        objetoEmprestimo = new Emprestimo();
+        objetoAmigo = new Amigo();
+        listaFerramenta = new ArrayList<Ferramenta>();
+        listaEmprestimo = new ArrayList<Emprestimo>();
+        listaAmigo = new ArrayList<Amigo>();
+    }
+
+    public String formataEmprestimo(Emprestimo e){
+        listaFerramenta = objetoFerramenta.getMinhaLista();
+        listaAmigo = objetoAmigo.getMinhaLista();
+        String relatorio = "";
+        for (Ferramenta f : listaFerramenta){
+            if(e.getIdFerramenta() == f.getId()){
+                relatorio = f.getNome()+" da marca "+f.getMarca()+" emprestada para ";
+            }
+        }
+        for (Amigo a : listaAmigo) {
+            if(e.getIdAmigo() == a.getId()){
+                relatorio = relatorio+a.getNome()+":\n"+e.getDataEmprestimo();
+            }
+        }
+        return relatorio;
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        JTARelatorio = new javax.swing.JTextArea();
+        JBCancelar = new javax.swing.JButton();
+        JBGerar = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        JTARelatorio.setColumns(20);
+        JTARelatorio.setRows(5);
+        JTARelatorio.setDisabledTextColor(new java.awt.Color(0, 0, 0));
+        JTARelatorio.setEnabled(false);
+        jScrollPane1.setViewportView(JTARelatorio);
+
+        JBCancelar.setText("Cancelar");
+        JBCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBCancelarActionPerformed(evt);
+            }
+        });
+
+        JBGerar.setText("Gerar");
+        JBGerar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBGerarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(JBCancelar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(JBGerar)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 92, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(JBCancelar)
+                    .addComponent(JBGerar))
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void JBCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBCancelarActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_JBCancelarActionPerformed
+
+    private void JBGerarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBGerarActionPerformed
+        listaEmprestimo = objetoEmprestimo.getMinhaLista();
+
+        String concluido = "Empréstimo concluidos\n\n";
+        String andamento = "Empréstimos em andamento\n\n";
+        String relatorio = "Relatório\n\n";
+        String info = "Informações adicionais\nMais Emprestimos\n\n"+objetoEmprestimo.identificaQtdEmprestimo()+"Amigos sem devoluções\n\n"+objetoEmprestimo.identificaSemDevolucao();
+        ArrayList<String> semDev = new ArrayList<String>();
+        
+        for (Emprestimo e : listaEmprestimo) {
+            if (e.getDataDevolucao() != null) {
+                concluido = concluido+formataEmprestimo(e)+" - "+e.getDataDevolucao()+"\n\n";
+            } else {
+                andamento = andamento+formataEmprestimo(e)+" - em andamento\n\n";
+            }
+        }
+        
+        
+        relatorio = relatorio+andamento+concluido+info;
+        
+        this.JTARelatorio.setText(relatorio);
+    }//GEN-LAST:event_JBGerarActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton JBCancelar;
+    private javax.swing.JButton JBGerar;
+    private javax.swing.JTextArea JTARelatorio;
+    private javax.swing.JScrollPane jScrollPane1;
+    // End of variables declaration//GEN-END:variables
+}
